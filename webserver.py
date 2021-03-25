1b9f05dc595eaa4ef720bbd5d204c26061780e5f
Tenosiey <tjark.froelje@gmail.com>
2021-03-25 14:18:12 +0100

    funktionuckelt

--- webserver.py
+++ webserver.py
@@ -1,3 +1,9 @@
 import os
 import basic
 
+basic_done = input("Wurde das basic.py Script bereits ausgeführt?")
+
+if basic_done == "y":
+    basic.basic_tools()
+elif basic_done == "n":
+    print("Dann nicht...")
